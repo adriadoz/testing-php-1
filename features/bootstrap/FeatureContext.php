41a9e557e88baf1e34a33fc76c6d8a2b49544246
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-04-18 20:40:17 +0200

    Change in the feature and add functions of the steps in FeatureContext.php

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -9,6 +9,8 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements Context
 {
+    private $lastResponse;
+    private $lastStatusCode;
     /**
      * Initializes context.
      *
@@ -18,5 +20,45 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given /^I do a "([^"]*)" request to "([^"]*)"$/
+     */
+    public function iDoARequestTo($method, $uri)
+    {
+        $client = new Client(['base_uri' => 'https://pokeapi.co/api/v2']);
+
+        try {
+            $request = $client->request($method, $uri);
+
+            $this->lastResponse   = $request->getBody()->getContents();
+            $this->lastStatusCode = $request->getStatusCode();
+        } catch (ClientException $exception) {
+            $this->lastResponse   = $exception->getResponse()->getBody();
+            $this->lastStatusCode = $exception->getResponse()->getStatusCode();
+        }
+    }
+
+    /**
+     * @Then /^the response code should be "([^"]*)"$/
+     */
+    public function theResponseCodeShouldBe($statusCode)
+    {
+        if ($this->lastStatusCode != $statusCode) {
+            throw new Exception('Status code is not the expected');
+        }
+    }
+
+    /**
+     * @Given /^the response should be:$/
+     */
+    public function theResponseShouldBe(PyStringNode $string)
+    {
+        $a = json_decode($string->getRaw());
+        $b = json_decode($this->lastResponse);
+        if (json_encode($a) !== json_encode($b)) {
+            throw new Exception('Response is not the same');
+        }
     }
 }
--- features/get_physical_attributes_pokemon.feature
+++ features/get_physical_attributes_pokemon.feature
@@ -4,10 +4,13 @@ Feature: Get Physical Attributes Pokemon
   I want to get the height and weight of some Pokemon
 
   Scenario: when pokemon do not exist
-    Given I do a "GET" request to "/pokemon?id=5438789879"
+    Given I do a "GET" request to "/pokemon/5438789879"
     Then the response code should be "404"
 
   Scenario: when pokemon exist
-    Given I do a "GET" request to "/pokemon?id=2"
+    Given I do a "GET" request to "/pokemon/2"
     Then the response code should be "200"
-    And  the response should be: """{"name":"voltorb","weight":5,"height":12}"""
+    And  the response should be:
+    """
+    {"name":"voltorb","weight":5,"height":12}
+    """
